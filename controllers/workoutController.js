684460e7b94655255161f16c87a5c59079ed5412
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-23 14:28:04 -0700

    implement get all workouts API

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -16,6 +16,20 @@ const createWorkout = async (req, res) => {
     }
 };
 
+const getWorkouts=async (req,res)=>{
+
+    try{
+        const workout = await Workout.find();
+
+        res.status(200).json(workouts);
+    } catch(error) {
+        res.status(500).json({
+            message:error.message,
+        })
+    }
+}
+
 module.exports = {
     createWorkout,
+    getWorkouts,
 };
--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const {
     createWorkout,
+    getWorkouts,
 } = require("../controllers/workoutController");
 
 const router = express.Router();
 
 router.post("/", createWorkout);
+router.get("/",getWorkouts);
 
 module.exports = router;
