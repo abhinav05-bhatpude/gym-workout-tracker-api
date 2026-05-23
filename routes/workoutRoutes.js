318c1244b0d8fbf8c908f4a481469b398cd5d9ec
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-23 14:09:13 -0700

    create workout API routes

---
+++ routes/workoutRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const {
+    createWorkout,
+} = require("../controllers/workoutController");
+
+const router = express.Router();
+
+router.post("/", createWorkout);
+
+module.exports = router;
