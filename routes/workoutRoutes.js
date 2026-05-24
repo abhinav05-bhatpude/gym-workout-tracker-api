a0a95c68c97e862c22019ba6e994c75379a110ad
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-24 13:51:35 -0700

    add delete workout API route

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -1,17 +1,19 @@
 const express = require("express");
 
 const {
-    createWorkout,
-    getWorkouts,
-    getWorkoutById,
-    updateWorkout,
+  createWorkout,
+  getWorkouts,
+  getWorkoutById,
+  updateWorkout,
+  deleteWorkout,
 } = require("../controllers/workoutController");
 
 const router = express.Router();
 
 router.post("/", createWorkout);
-router.get("/",getWorkouts);
-router.get("/:id",getWorkoutById);
-router.put("/:id",updateWorkout);
+router.get("/", getWorkouts);
+router.get("/:id", getWorkoutById);
+router.put("/:id", updateWorkout);
+router.delete("/:id", deleteWorkout);
 
 module.exports = router;
