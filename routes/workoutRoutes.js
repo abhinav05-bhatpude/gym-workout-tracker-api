d3bb8f814005ccc89ab7f49ce505a0fbdf44d69c
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-24 13:35:58 -0700

    add update workout API route

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -3,11 +3,15 @@ const express = require("express");
 const {
     createWorkout,
     getWorkouts,
+    getWorkoutById,
+    updateWorkout,
 } = require("../controllers/workoutController");
 
 const router = express.Router();
 
 router.post("/", createWorkout);
 router.get("/",getWorkouts);
+router.get("/:id",getWorkoutById);
+router.put("/:id",updateWorkout);
 
 module.exports = router;
