17257dfebfa33eb55519aff08a934a7d8a4c0809
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-25 12:50:45 -0700

    add request logging middleware

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req,res,next) => {
+
+    console.log(`${req.method} ${req.url}`);
+
+    next();
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
